91479961c04dd3fe0bb3a9a505b3031ccc8a29ad
Loucas Stylianou <loukkis91@gmail.com>
2020-05-16 12:41:47 +0300

    Added create tag feature

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
+from django.conf import settings
+
 
 class UserManager(BaseUserManager):
 
@@ -8,25 +10,26 @@ class UserManager(BaseUserManager):
         """Creates and saves a new user"""
         if email is None:
             raise ValueError('Users must have an email address')
-        user = self.model(email=self.normalize_email(email),**extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        #supporting multiple db
+        # supporting multiple db
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email,password):
+    def create_superuser(self, email, password):
         """Creates and saves a new super user"""
-        user = self.create_user(email,password)
+        user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
 
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
-    email = models.EmailField(max_length=255,unique=True)
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -34,3 +37,14 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Tag(models.Model):
+    """Tag to be used for a recipe"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
--- app/manage.py
+++ app/manage.py
@@ -4,6 +4,17 @@ import sys
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+    if os.environ.get("VSCODE_DEBUGGER", False):
+        import ptvsd
+
+        ptvsd_port = os.environ.get("PORT_PTVSD", 5678)
+
+        try:
+            ptvsd.enable_attach(address=("0.0.0.0", ptvsd_port))
+            print("Started ptvsd at port %s." % ptvsd_port)
+        except OSError:
+            print("ptvsd port %s already in use." % ptvsd_port)
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
