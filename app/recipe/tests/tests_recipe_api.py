adceaca65df0242b1740a4ff0f0f684fd3bb2992
Loucas Stylianou <loukkis91@gmail.com>
2020-05-16 17:41:53 +0300

    Added tests for recipe update (PATCH,PUT) APIs

--- app/recipe/tests/tests_recipe_api.py
+++ app/recipe/tests/tests_recipe_api.py
@@ -157,3 +157,43 @@ class PrivateRecipeApiTests(TestCase):
         self.assertEqual(ingredients.count(),2)
         self.assertIn(ingredient11,ingredients)
         self.assertIn(ingredient12,ingredients)
+
+
+    def test_recipe_partial_update(self):
+        """Test updating a recipe with PATCH"""
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        new_tag = sample_tag(user=self.user,name='Curry')
+
+        payload = {
+            'title': 'Chicke tikka',
+            'tags': [new_tag.id]
+        }
+        url = detail_url(recipe.id)
+        res = self.client.patch(url,payload)
+        #refresh the object details since it is updated through rest api client
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.title, payload['title'])
+        tags = recipe.tags.all()
+        self.assertEqual(len(tags),1)
+        self.assertIn(new_tag,tags)
+
+
+
+    def test_recipe_full_update(self):
+        """Test updating a recipe with PUT"""
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        payload = {
+            'title' : 'Spaghetti carbonara',
+            'time_minutes' : 20,
+            'price': 5.00
+        }
+        url = detail_url(recipe.id)
+        res = self.client.put(url,payload)
+
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.title,payload['title'])
+        self.assertEqual(recipe.time_minutes,payload['time_minutes'])
+        self.assertEqual(recipe.price, payload['price'])
+        self.assertEqual(recipe.tags.all().count(),0)
